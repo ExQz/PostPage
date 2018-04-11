415f009e4311ce43bd84d7726ca4d53a34d15ef0
Marius <marius.blazys16@gmail.com>
2018-04-11 21:29:00 +0300

    .net watch and models added

---
+++ backend/backend/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+
+        public string CommentAuthor { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public long PostId { get; set; }
+
+    }
+}
---
+++ backend/backend/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Post
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int CommentCount { get; set; }
+
+    }
+}
